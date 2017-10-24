afbc563e066f9800c2a63b1d7d43739a390cb92c
Malte Bossert <alu0101182773@ull.edu.es>
2017-10-24 15:45:46 +0100

    Test fails, class doesn't have the required instance variables

--- spec/nutrientes_spec.rb
+++ spec/nutrientes_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 require "nutrientes"
 
-RSpec.describe Nutrientes do
+RSpec.describe Alimento do
 
   before :each do
     @choco = Alimento.new("Chocolate", 5.3, 47.0, 30.0)
@@ -15,7 +15,23 @@ RSpec.describe Nutrientes do
 
   end
 
-  #describe "basic properties of a food" do
+  describe "basic properties of a food" do
 
-  #end
+    it "has the property nombre" do
+      expect(@choco.instance_variable_defined?("@nombre")).to eq(true)
+    end
+
+    it "has the property proteinas" do
+      expect(@choco.instance_variable_defined?("@proteinas")).to eq(true)
+    end
+    
+    it "has the property glucidos" do
+      expect(@choco.instance_variable_defined?("@glucidos")).to eq(true)
+    end
+
+    it "has the property grasas" do
+      expect(@choco.instance_variable_defined?("@grasas")).to eq(true)
+    end
+
+  end
 end
