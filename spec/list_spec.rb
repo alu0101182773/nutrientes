9202ba66829a5451067abe097b957b456e975417
Malte Bossert <alu0101182773@ull.edu.es>
2017-11-07 14:07:50 +0000

    Added Element to the list

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -3,6 +3,19 @@ require 'nutrientes'
 
 RSpec.describe List do
 
+  context "Basic List" do
 
+    it "can create a new list" do
+
+      list = List.new()
+
+    end
+
+    it "can put an Element into the list" do
+      list = List.new()
+      list.add(42)
+    end
+
+  end   
 
 end
